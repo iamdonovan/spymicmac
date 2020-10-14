ea4b563639b0995433127e254df1feb1d4502da0
Robert McNabb <robertmcnabb@gmail.com>
2020-10-14 10:39:46 +0200

    make specifying origin optional, attempt to find image frame and search from there.

--- bin/find_reseau_shifts.py
+++ bin/find_reseau_shifts.py
@@ -4,13 +4,15 @@ import argparse
 import cv2
 import multiprocessing as mp
 from functools import partial
-from scipy.ndimage.filters import median_filter
+from PIL import Image
+from shapely.geometry import LineString
 from skimage.feature import peak_local_max
+from skimage.morphology import disk
+from skimage import filters
 from scipy.interpolate import griddata
 import numpy as np
 import matplotlib.pyplot as plt
 import gdal
-# import pyvips
 import pandas as pd
 import lxml.etree as etree
 import lxml.builder as builder
@@ -18,6 +20,12 @@ from pybob.bob_tools import mkdir_p
 from pybob.ddem_tools import nmad
 import sPyMicMac.image_tools as imtools
 import sPyMicMac.micmac_tools as mmt
+
+
+def moving_average(a, n=5):
+    ret = np.cumsum(a)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
 
 
 def lsq_fit(x, y, z):
@@ -43,52 +51,73 @@ def lsq_fit(x, y, z):
     return out_lsq
 
 
+def find_cross(_img, _pt, _cross, _args):
+    _subimg, _, _ = imtools.make_template(_img, _pt, _args.tsize)
+    res, this_i, this_j = imtools.find_match(_subimg, _cross)
+    inv_res = res.max() - res
+
+    pks = peak_local_max(inv_res, min_distance=5, num_peaks=2)
+    this_z_corrs = []
+    for pk in pks:
+        max_ = inv_res[pk[0], pk[1]]
+        this_z_corrs.append((max_ - inv_res.mean()) / inv_res.std())
+    if max(this_z_corrs) > 4 and max(this_z_corrs)/min(this_z_corrs) > 1.15:
+        return this_i-_args.tsize+_pt[0], this_j-_args.tsize+_pt[1]
+    else:
+        return np.nan, np.nan
+
+
 def get_im_meas(gcps, E):
     pt_els = []
     for ind, row in gcps.iterrows():
         this_mes = E.OneMesureAF1I(
-                        E.NamePt(row['gcp']),
-                        E.PtIm('{} {}'.format(row['im_col'], row['im_row']))
+                        E.NamePt(row['name']),
+                        E.PtIm('{} {}'.format(row['match_j'], row['match_i']))
                         )
         pt_els.append(this_mes)
     return pt_els
 
 
-def make_grid():
-    pass
+def make_grid(ledge, redge):
+    i_grid = []
+    j_grid = []
+    search_pts = []
+
+    for ii in range(0, 23):
+        this_left = ledge.interpolate(ii * left_edge.length / 22)
+        this_right = redge.interpolate(ii * right_edge.length / 22)
+        this_line = LineString([this_left, this_right])
+        for jj in range(0, 24):
+            this_pt = this_line.interpolate(jj * this_line.length / 23)
+            i_grid.append(ii)
+            j_grid.append(jj)
+            search_pts.append((this_pt.y, this_pt.x))
+
+    return i_grid, j_grid, search_pts
 
 
-def get_grid_matches():
-    pass
-
-
-def warp_image():
-    pass
+def get_perp(corners):
+    a = np.array([corners[1][0] - corners[0][0],
+                  corners[1][1] - corners[0][1]])
+    a = a / np.linalg.norm(a)
+    return np.array([-a[1], a[0]])
 
 
 def _argparser():
-    parser = argparse.ArgumentParser(description="Find Reseau marks and warp Hexagon image.",
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('img', action='store', type=str, help='Image to warp.')
-    # parser.add_argument('right_img', action='store', type=str, help='Right image to warp.')
-    parser.add_argument('-orig', action='store', nargs=2, type=float, default=None,
-                        help='Location of origin to search from in image [row, column]')
-    # parser.add_argument('-r_orig', action='store', nargs=2, type=int, default=None,
-    #                    help='Location of origin to search from in right image [row, column]')
-    parser.add_argument('-csize', action='store', type=int, default=361, help='cross size [361 pixels]')
-    parser.add_argument('-tsize', action='store', type=int, default=300, help='half-size of search window [300 pixels]')
-    parser.add_argument('-scanres', action='store', type=float, default=None,
-                        help='Scanning resolution in pixels per cm [None]')
-    parser.add_argument('-scanres_y', action='store', type=float, default=None,
-                        help='Scanning resolution in pixels per cm in y direction [None]')
-    parser.add_argument('-filt_size', action='store', type=int, default=5, help='Size of median filter to use [5x5]')
-    parser.add_argument('-nproc', action='store', type=int, default=1,
-                        help='Number of processors to use [1].')
-    parser.add_argument('-joined', action='store_true',
-                        help='Use if img represents an already re-joined image.')
-    parser.add_argument('-s', '--save_outputs', action='store_true',
-                        help='Save image showing location of match for each reseau mark.')
-    return parser
+    _parser = argparse.ArgumentParser(description="Find Reseau marks and warp Hexagon image.",
+                                      formatter_class=argparse.RawDescriptionHelpFormatter)
+    _parser.add_argument('img', action='store', type=str, help='Image to warp.')
+    _parser.add_argument('-orig', action='store', nargs=2, type=float, default=None,
+                         help='Location of origin to search from in image [row, column]')
+    _parser.add_argument('-csize', action='store', type=int, default=361, help='cross size [361 pixels]')
+    _parser.add_argument('-tsize', action='store', type=int, default=300, help='half-size of search window [300 pixels]')
+    _parser.add_argument('-scanres', action='store', type=float, default=None,
+                         help='Scanning resolution in pixels per cm [None]')
+    _parser.add_argument('-scanres_y', action='store', type=float, default=None,
+                         help='Scanning resolution in pixels per cm in y direction [None]')
+    _parser.add_argument('-nproc', action='store', type=int, default=1,
+                         help='Number of processors to use [1].')
+    return _parser
 
 
 # def main():
@@ -96,102 +125,98 @@ np.seterr(divide='ignore', invalid='ignore')
 parser = _argparser()
 args = parser.parse_args()
 
-# outname = os.path.splitext(args.left_img)[0].split('_')[0] + '_shift' + os.path.splitext(args.left_img)[-1]
-
 print('Reading {}'.format(args.img))
-
 gd = gdal.Open(args.img)
 metadata = gd.GetMetadata_Dict()
 img = gd.ReadAsArray()
 print('Image read.')
 
-if args.orig is None:
-    raise ValueError('Origin must be provided for image.')
+img_ = Image.fromarray(img)
+img_lowres = np.array(img_.resize((np.array(img_.size)/10).astype(int), Image.LANCZOS))
+del img_
 
 tmp_cross = imtools.cross_template(args.csize, width=3)
-# cross = np.random.randint(0, 255, size=(361, 361)).astype(np.uint8)
 cross = np.ones((args.csize, args.csize)).astype(np.uint8)
 cross[tmp_cross == 1] = 255
 
-orig_y, orig_x = args.orig
-subimg, _, _ = imtools.make_template(img, args.orig, args.tsize)
-# find the origin in the given image
-z_corrs = []
-dz_corrs = []
+fig = plt.figure(figsize=(7, 12))
+ax = fig.add_subplot(111)
+ax.imshow(img_lowres, cmap='gray', extent=[0, img.shape[1], img.shape[0], 0])
 
-res, orig_i, orig_j = imtools.find_match(subimg, cross)
-maxj, maxi = cv2.minMaxLoc(res)[2]  # 'peak' is actually the minimum location, remember.
-inv_res = res.max() - res
+if args.orig is None:
+    img_lowres_mask = np.zeros(img_lowres.shape)
+    img_lowres_mask[np.logical_or(img_lowres < 25,
+                                  filters.median(img_lowres, selem=disk(16)) < 25)] = 1
 
-pks = peak_local_max(inv_res, min_distance=5, num_peaks=2)
-this_z_corrs = []
-for pk in pks:
-    max_ = inv_res[pk[0], pk[1]]
-    this_z_corrs.append((max_ - inv_res.mean()) / inv_res.std())
-dz_corrs.append(max(this_z_corrs) / min(this_z_corrs))
-z_corrs.append(max(this_z_corrs))
-if max(this_z_corrs) > 5 and max(this_z_corrs)/min(this_z_corrs) > 1.15:
-    oi, oj = orig_i-args.tsize+orig_y, orig_j-args.tsize+orig_x
-    print('Found origin location: {}, {}'.format(oi, oj))
-else:
-    print('Failed to find valid origin. Falling back to the one provided.')
-    oi, oj = orig_y, orig_x
+    vert = np.count_nonzero(filters.sobel_v(img_lowres_mask)**2 > 0.8, axis=0)
+    hori = np.count_nonzero(filters.sobel_h(img_lowres_mask)**2 > 0.8, axis=1)
 
-this_res_list = [res]
+    vpeaks = peak_local_max(moving_average(vert), min_distance=6000, num_peaks=1, exclude_border=10)
+    hpeaks = peak_local_max(moving_average(hori), min_distance=3000, num_peaks=2, exclude_border=10)
 
-if args.save_outputs:
-    mkdir_p('gcp_imgs')
-    plt.figure(figsize=(8,8))
-    plt.imshow(subimg, cmap='gray')
-    plt.plot(orig_j, orig_i, 'r+', ms=8, linewidth=2)
-    plt.savefig('gcp_imgs/{}_match_0_0.png'.format(os.path.splitext(args.img)[0]), bbox_inches='tight', dpi=200)
-    plt.close()
-
-if args.scanres is None:
-    npix_x = np.float(metadata['TIFFTAG_XRESOLUTION'])
-    npix_y = np.float(metadata['TIFFTAG_YRESOLUTION'])
-else:
-    npix_x = args.scanres
-    if args.scanres_y is None:
-        npix_y = args.scanres
+    top, bot = 10 * hpeaks.min(), 10 * hpeaks.max()
+    edge = 10 * vpeaks.min()
+    if edge < 5000:
+        search_corners = [(bot-625, edge+180), (top+625, edge+180)]
     else:
-        npix_y = args.scanres_y
+        search_corners = [(bot - 625, edge-180), (top + 625, edge-180)]
 
-# now, go every 10mm in x, y direction until we cover the image.
-# should have 47 marks across, 23 up
-i_list = -npix_y * np.arange(22, -1, -1) + oi
-# make j variable based on origin location, size of image
-# if img.shape[1] > 40000:
-#    j_list = npix * np.arange(0, 47) + oj
-# else:
-if not args.joined:
-    full_j = 23 * npix_x + oj < img.shape[1]
+    # corners = [(bot, lft), (bot, rgt), (top, rgt), (top, lft)]
+    # search_corners = [(bot-625, lft+180), (bot-625, rgt-180),
+    #                   (top+625, rgt-180), (top+625, lft+180)] # average distance of cross centers to img corners
+    grid_corners = []
+    for c in search_corners:
+        _i, _j = find_cross(img, c, cross, args)
+        if any(np.isnan([_j, _i])):
+            grid_corners.append((c[1], c[0]))
+        else:
+            grid_corners.append((_j, _i))
+    pixres = (grid_corners[0][1] - grid_corners[1][1]) / 22
+    npix = 23 * pixres
 
-    if full_j:
-        j_list = npix_x * np.arange(0, 24) + oj
+    perp = get_perp(grid_corners)
+
+    if edge < 5000:
+        left_edge = LineString([grid_corners[0], grid_corners[1]])
+        right_edge = LineString([grid_corners[0] + npix * perp,
+                                 grid_corners[1] + npix * perp])
     else:
-        j_list = npix_x * np.arange(0, 23) + oj
+        right_edge = LineString([grid_corners[0], grid_corners[1]])
+        left_edge = LineString([grid_corners[0] - npix * perp,
+                                grid_corners[1] - npix * perp])
+
 else:
-    j_list = npix_x * np.arange(0,47) + oj
+    orig_y, orig_x = args.orig
+    oj, oi = find_cross(img, args.orig, cross, args)
+    if any(np.isnan([oj, oi])):
+        print('Failed to find valid origin. Falling back to the one provided.')
+        oi, oj = orig_y, orig_x
+    else:
+        print('Found origin location: {}, {}'.format(oi, oj))
 
-nj = j_list.size
+    if args.scanres is None:
+        npix_x = np.float(metadata['TIFFTAG_XRESOLUTION'])
+        npix_y = np.float(metadata['TIFFTAG_YRESOLUTION'])
+    else:
+        npix_x = args.scanres
+        if args.scanres_y is None:
+            npix_y = args.scanres
+        else:
+            npix_y = args.scanres_y
 
-J, I = np.meshgrid(np.arange(0, j_list.size), np.arange(0, i_list.size))
-gcp_names = list(zip(I[0, :], J[0, :]))
-for i in range(1, i_list.size):
-    gcp_names.extend(list(zip(I[i, :], J[i, :])))
+    left_edge = LineString([(oj, oi), (oj, oi+22*npix_y)])
+    right_edge = LineString([(oj+23*npix_x, oi), (oj+23*npix_x, oi+23*npix_y)])
 
-JJ, II = np.meshgrid(np.round(j_list).astype(int), np.round(i_list).astype(int))
-ij = list(zip(II[-1, :], JJ[-1, :]))
-for i in range(21, -1, -1):
-    ij.extend(list(zip(II[i, :], JJ[i, :])))
+II, JJ, search_grid = make_grid(left_edge, right_edge)
+matched_grid = []
 
 print('Finding grid points in image...')
 # now that we have a list of coordinates, we can loop through and find the real grid locations
-warped_ij = [(oi, oj)] # using oi, oj to preserve float values
 if args.nproc > 1:
     subimgs = []
-    for loc in ij[1:]:
+    std_devs = []
+    means = []
+    for loc in search_grid:
         subimg, _, _ = imtools.make_template(img, loc, args.tsize)
         subimgs.append(subimg)
     pool = mp.Pool(args.nproc)
@@ -202,123 +227,77 @@ if args.nproc > 1:
         res, this_i, this_j = output
         maxj, maxi = cv2.minMaxLoc(res)[2]  # 'peak' is actually the minimum location, remember.
         inv_res = res.max() - res
+        std_devs.append(inv_res.std())
+        means.append(inv_res.mean())
 
         pks = peak_local_max(inv_res, min_distance=5, num_peaks=2)
-        this_z_corrs = []
-        for pk in pks:
-            max_ = inv_res[pk[0], pk[1]]
-            this_z_corrs.append((max_ - inv_res.mean()) / inv_res.std())
-        dz_corrs.append(max(this_z_corrs) / min(this_z_corrs))
-        z_corrs.append(max(this_z_corrs))
+        if pks.size > 0:
+            this_z_corrs = []
+            for pk in pks:
+                max_ = inv_res[pk[0], pk[1]]
+                this_z_corrs.append((max_ - inv_res.mean()) / inv_res.std())
 
-        if max(this_z_corrs) > 5 and max(this_z_corrs)/min(this_z_corrs) > 1.15:
-            this_res_list.append(res)
-            rel_i = this_i - args.tsize
-            rel_j = this_j - args.tsize
-            i, j = ij[n+1]
-            warped_ij.append((rel_i+i, rel_j+j))
+            if max(this_z_corrs) > 5 and max(this_z_corrs)/min(this_z_corrs) > 1.15:
+                rel_i = this_i - args.tsize
+                rel_j = this_j - args.tsize
+                i, j = search_grid[n]
+                matched_grid.append((rel_i+i, rel_j+j))
+            else:
+                matched_grid.append((np.nan, np.nan))
         else:
-            this_res_list.append(res)
-            warped_ij.append((np.nan, np.nan))
-
-        if args.save_outputs:
-            plt.figure(figsize=(8,8))
-            plt.imshow(subimgs[n], cmap='gray')
-            plt.plot(this_j, this_i, 'r+', ms=8, linewidth=2)
-            plt.savefig('gcp_imgs/{}_match_{}_{}.png'.format(os.path.splitext(args.img)[0], gcp_names[n+1][0],
-                                                             gcp_names[n+1][1]), bbox_inches='tight', dpi=200)
-            plt.close()
+            matched_grid.append((np.nan, np.nan))
 else:
-    for loc in ij[1:]:
-        i, j = loc
-        subimg, _, _ = imtools.make_template(img, loc, args.tsize)
-        # img_eq = rank.equalize(subimg, selem=selem)
-        res, this_i, this_j = imtools.find_match(subimg, cross)
-        warped_ij.append((this_i+i, this_j+j))
+    for loc in search_grid:
+        _j, _i = find_cross(img, loc, cross, args)
+        matched_grid.append((_j, _i))
 
-# res_list.append(this_res_list)
-ij = np.array(ij)
-warped_ij = np.array(warped_ij)
-warp = warped_ij - ij
+matched_grid = np.array(matched_grid)
+search_grid = np.array(search_grid)
 
-ux = np.reshape(warp[:,1], (23, nj))
-uy = np.reshape(warp[:,0], (23, nj))
+gcps_df = pd.DataFrame()
+for i, pr in enumerate(list(zip(II, JJ))):
+    gcps_df.loc[i, 'gcp'] = 'GCP_{}_{}'.format(pr[0], pr[1])
 
-ux_lsq = lsq_fit(JJ, II, ux)
-uy_lsq = lsq_fit(JJ, II, uy)
+gcps_df['search_j'] = search_grid[:, 1]
+gcps_df['search_i'] = search_grid[:, 0]
+gcps_df['match_j'] = matched_grid[:, 1]
+gcps_df['match_i'] = matched_grid[:, 0]
 
-ux_detrend = ux - ux_lsq
-uy_detrend = uy - uy_lsq
+dx = gcps_df['match_j'] - gcps_df['search_j']
+dy = gcps_df['match_i'] - gcps_df['search_i']
 
-ux[np.abs(ux_detrend) > 5 * nmad(ux_detrend)] = np.nan
-uy[np.abs(uy_detrend) > 5 * nmad(uy_detrend)] = np.nan
+nomatch = np.isnan(gcps_df.match_j)
 
-ux[np.isnan(ux)] = ux_lsq[np.isnan(ux)]
-uy[np.isnan(uy)] = uy_lsq[np.isnan(uy)]
+# ux = griddata(gcps_df.loc[~nomatch, ['search_j', 'search_i']], gcps_df.loc[~nomatch, 'dj'],
+#               gcps_df[['search_j', 'search_i']])
+# uy = griddata(gcps_df.loc[~nomatch, ['search_j', 'search_i']], gcps_df.loc[~nomatch, 'di'],
+#               gcps_df[['search_j', 'search_i']])
 
-# if args.filt_size > 0:
-#     ux_ = median_filter(ux, size=args.filt_size, mode='nearest')
-#     uy_ = median_filter(uy, size=args.filt_size, mode='nearest')
-#     # see how much the vectors changed after the median filter
-#     dx_ = ux - ux_
-#     dy_ = uy - uy_
-#     # unless there are huge deflections, keep the original values
-#     ux_[np.abs(dx_) < 5] = ux[np.abs(dx_) < 5]
-#     uy_[np.abs(dy_) < 5] = uy[np.abs(dy_) < 5]
-# else:
-#     ux_ = ux
-#     uy_ = uy
+ux = lsq_fit(gcps_df.search_j.values, gcps_df.search_i.values, gcps_df.match_j.values)
+uy = lsq_fit(gcps_df.search_j.values, gcps_df.search_i.values, gcps_df.match_i.values)
 
-# ux_interp = griddata((J[np.isfinite(ux_)], I[np.isfinite(ux_)]), ux_[np.isfinite(ux_)], (J, I))
-# uy_interp = griddata((J[np.isfinite(uy_)], I[np.isfinite(uy_)]), uy_[np.isfinite(uy_)], (J, I))
+gcps_df.loc[nomatch, 'match_j'] = ux[nomatch]
+gcps_df.loc[nomatch, 'match_i'] = uy[nomatch]
 
-map_x = ij[:,1]
-map_y = ij[:,0]
-image_x = map_x + ux.reshape(-1)
-image_y = map_y + uy.reshape(-1)
-# image_x = warped_ij[:,1]
-# image_y = warped_ij[:,0]
+gcps_df['dj'] = gcps_df['match_j'] - gcps_df['search_j']
+gcps_df['di'] = gcps_df['match_i'] - gcps_df['search_i']
 
-# make sure that the first point is the same - no shift for the origin point.
-image_x[0] = map_x[0]
-image_y[0] = map_y[0]
-
-# warp_list.append(warp)
-# ij_list.append(ij)
-# img_xy_list.append(np.column_stack((image_x,image_y)))
+gcps_df['im_row'] = gcps_df['match_i']
+gcps_df['im_col'] = gcps_df['match_j']
 
 print('Grid points found.')
-# print('Warping {} left image.'.format(name_list[ind]))
-# generate a transform using the reseau field
-# t = tf.PolynomialTransform()
-# t.estimate(warped_ij, ij)
+mkdir_p('match_imgs')
 
-# apply the transformation with bicubic resampling
-# warped_img = tf.warp(img_list[0], t, order=3)
-
-# warped_list.append(warped_img)
-# save a results image so we can see if there's anything suspicious
-plt.figure(figsize=(12,12))
-plt.imshow(img[::10, ::10], extent=[0, img.shape[1], img.shape[0], 0], cmap='gray')
-plt.quiver(image_x, image_y, -ux.reshape(-1), -uy.reshape(-1), color='r')
-# plt.quiver(map_x, map_y, warp[:,1], warp[:,0], color='r')
-#
+ax.quiver(gcps_df.search_j, gcps_df.search_i, gcps_df.dj, gcps_df.di, color='r')
+ax.plot(gcps_df.search_j[nomatch], gcps_df.search_i[nomatch], 'b+')
 this_out = os.path.splitext(args.img)[0]
-plt.savefig(this_out + '_matches.png', bbox_inches='tight', dpi=200)
-plt.close()
-#
-# gcp_list = []
-gcp_df = pd.DataFrame()
-for i, ind in enumerate(gcp_names):
-    row, col = ind
-    gcp_df.loc[i, 'gcp'] = 'GCP_{}_{}'.format(row, col)
-    gcp_df.loc[i, 'im_row'] = image_y[i]
-    gcp_df.loc[i, 'im_col'] = image_x[i]
+fig.savefig('match_imgs/' + this_out + '_matches.png', bbox_inches='tight', dpi=200)
+# fig.close()
 
 E = builder.ElementMaker()
 ImMes = E.MesureAppuiFlottant1Im(E.NameIm(args.img))
 
-pt_els = mmt.get_im_meas(gcp_df, E)
+pt_els = mmt.get_im_meas(gcps_df, E)
 for p in pt_els:
     ImMes.append(p)
 mkdir_p('Ori-InterneScan')
