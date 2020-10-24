654b671e206acb36878cd64b5ef6b1fe9ef3686b
Bob McNabb <robertmcnabb@gmail.com>
2020-10-24 15:12:12 +0100

    switch blend datatype to float32, add ability to join KH-4 images.

--- bin/join_hexagon_halves.py
+++ bin/join_hexagon_halves.py
@@ -11,12 +11,56 @@ import numpy as np
 import sPyMicMac.image_tools as imtools
 
 
+def blend_halves(_left, _right, left_shape):
+    first = np.where(np.sum(_right, axis=0) > 0)[0][0]
+    last = left_shape[1]
+
+    m = 1 / (first - last)
+    alpha = np.ones(_left.shape, dtype=np.float32)
+    alpha[:, last:] = 0
+    for i, ind in enumerate(np.arange(first, last)):
+        alpha[:, ind] = 1 + m * (ind - first)
+
+    return alpha * _left + (1 - alpha) * _right
+
+
+def find_transform(left, right, args):
+    src_pts = []
+    dst_pts = []
+
+    row_inds = list(range(0, left.shape[0] + 1, args.overlap))
+    if row_inds[-1] != left.shape[0]:
+        row_inds.append(-1)
+    row_inds = np.array(row_inds)
+
+    for i, ind in enumerate(row_inds[:-1]):
+        try:
+            l_sub = left[ind:row_inds[i + 1], -args.overlap:]
+            r_sub = right[ind:row_inds[i + 1], :args.overlap]
+
+            kp, des, matches = imtools.get_matches(l_sub, r_sub)
+            src_pts.extend(
+                [np.array(kp[0][m.queryIdx].pt) + np.array([left.shape[1] - args.overlap, ind]) for m in matches])
+            dst_pts.extend([np.array(kp[1][m.trainIdx].pt) + np.array([0, ind]) for m in matches])
+        except:
+            continue
+
+    M, inliers = ransac((np.array(src_pts), np.array(dst_pts)), EuclideanTransform,
+                        min_samples=25, residual_threshold=1, max_trials=1000)
+    print('{} tie points found'.format(np.count_nonzero(inliers)))
+    return M
+
+
 def _argparser():
     parser = argparse.ArgumentParser(description="", formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-o', '--overlap', action='store', type=int, default=2000,
                         help='overlap search width between two images [2000]')
     parser.add_argument('-p', '--pattern', action='store', type=str, default='DZB',
                        help='Match pattern for images [DZB]')
+    parser.add_argument('-b', '--blend', action='store_true',
+                        help='Blend across image halves to prevent a sharp line at edge.')
+    parser.add_argument('-c', '--corona', action='store_true',
+                        help='Images are Corona KH-4/KH-4A scans (i.e., there are 4 parts).')
     return parser
 
 
@@ -27,70 +71,53 @@ imlist = [f.split('_a.tif')[0] for f in glob('{}*a.tif'.format(args.pattern))]
 for im in imlist:
     print(im)
 
-    left = imread('{}_a.tif'.format(im))
-    right = imread('{}_b.tif'.format(im))
+    if not args.corona:
+        left = imread('{}_a.tif'.format(im))
+        right = imread('{}_b.tif'.format(im))
 
-    left_gd = gdal.Open('{}_a.tif'.format(im))
-    right_gd = gdal.Open('{}_b.tif'.format(im))
+        left_gd = gdal.Open('{}_a.tif'.format(im))
+        right_gd = gdal.Open('{}_b.tif'.format(im))
 
-    # left_meta = left_gd.GetMetadata_Dict()
-    # right_meta = right_gd.GetMetadata_Dict()
+        M = find_transform(left, right, args)
 
-    # xres = np.mean(np.array([left_meta['TIFFTAG_XRESOLUTION'],
-    #                          right_meta['TIFFTAG_XRESOLUTION']]).astype(np.float32))
-    # yres = np.mean(np.array([left_meta['TIFFTAG_YRESOLUTION'],
-    #                          right_meta['TIFFTAG_YRESOLUTION']]).astype(np.float32))
+        out_shape = (left.shape[0], left.shape[1] + right.shape[1])
 
-    src_pts = []
-    dst_pts = []
+        combined_right = warp(right, M, output_shape=out_shape, preserve_range=True, order=3)
 
-    row_inds = list(range(0, left.shape[0]+1, args.overlap))
-    if row_inds[-1] != left.shape[0]:
-        row_inds.append(-1)
-    row_inds = np.array(row_inds)
+        combined_left = np.zeros(out_shape)
+        combined_left[:, :left.shape[1]] = left
 
-    for i, ind in enumerate(row_inds[:-1]):
-        try:
-            l_sub = left[ind:row_inds[i+1], -args.overlap:]
-            r_sub = right[ind:row_inds[i+1], :args.overlap]
+        if args.blend:
+            combined = blend_halves(combined_left, combined_right, left.shape)
+        else:
+            combined = combined_left + combined_right
 
-            kp, des, matches = imtools.get_matches(l_sub, r_sub)
-            src_pts.extend([np.array(kp[0][m.queryIdx].pt) + np.array([left.shape[1]-args.overlap, ind]) for m in matches])
-            dst_pts.extend([np.array(kp[1][m.trainIdx].pt) + np.array([0, ind]) for m in matches])
-        except:
-            continue
+        last_ind = np.where(np.sum(combined, axis=0) > 0)[0][-1]
+        combined = combined[:, :last_ind+1]
 
-    M, inliers = ransac((np.array(src_pts), np.array(dst_pts)), EuclideanTransform,
-                        min_samples=25, residual_threshold=1, max_trials=1000)
-    print('{} tie points found'.format(np.count_nonzero(inliers)))
+        imsave('{}.tif'.format(im), combined.astype(np.uint8))
+    else:
+        left = imread('{}_d.tif'.format(im))
+        for right_img in ['c', 'b', 'a']:
+            right = imread('{}_{}.tif'.format(im, right_img))
 
-    out_shape = (left.shape[0], left.shape[1] + right.shape[1])
+            M = find_transform(left, right, args)
+            out_shape = (left.shape[0], left.shape[1] + right.shape[1])
 
-    combined_right = warp(right, M, output_shape=out_shape, preserve_range=True, order=3)
+            combined_right = warp(right, M, output_shape=out_shape, preserve_range=True, order=3)
 
-    combined_left = np.zeros(out_shape)
-    combined_left[:, :left.shape[1]] = left
+            combined_left = np.zeros(out_shape)
+            combined_left[:, :left.shape[1]] = left
 
-    first = np.where(np.sum(combined_right, axis=0) > 0)[0][0]
-    last = left.shape[1]
+            if args.blend:
+                combined = blend_halves(combined_left, combined_right, left.shape)
+            else:
+                combined = combined_left + combined_right
 
-    m = 1 / (first-last)
-    alpha = np.ones(out_shape)
-    alpha[:, last:] = 0
-    for i, ind in enumerate(np.arange(first, last)):
-        alpha[:, ind] = 1 + m * (ind-first)
-    # combined_left[combined_left == 0] = np.nan
-    # combined_right[combined_right == 0] = np.nan
+            last_ind = np.where(np.sum(combined, axis=0) > 0)[0][-1]
+            left = combined[:, :last_ind + 1]
 
-    # combined = np.nansum(np.array([alpha * combined_left,
-    #                               (1-alpha) * combined_right]), axis=0)
-    # combined[np.isnan(combined)] = 0
-    combined = alpha * combined_left + (1-alpha) * combined_right
-
-    last_ind = np.where(np.sum(combined, axis=0) > 0)[0][-1]
-    combined = combined[:, :last_ind+1]
-
-    imsave('{}.tif'.format(im), combined.astype(np.uint8))
+        imsave('{}.tif'.format(im), combined.astype(np.uint8))
 
     # out_gd = gdal.Open('{}.tif'.format(im), gdal.GA_Update)
     # out_meta = out_gd.GetMetadata_Dict()
@@ -104,3 +131,6 @@ for im in imlist:
     # img_bal = img.hist_equal()
     # img_bal.write_to_file('{}.tif'.format(im))
     # os.remove('tmp.tif')
+
+# if __name__ == "__main__":
+#     main()
