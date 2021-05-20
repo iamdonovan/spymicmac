3e302c04cead31b2c08ebd79af0032967814291c
Bob McNabb <robertmcnabb@gmail.com>
2021-05-20 23:14:44 +0100

    change to write to new folder, balanced, instead of in-place.

--- bin/balance_images.py
+++ bin/balance_images.py
@@ -1,13 +1,20 @@
+import os
 from glob import glob
 from skimage.io import imread, imsave
 from skimage.exposure import equalize_adapthist
 from skimage.filters import median
 from skimage.morphology import disk
 import numpy as np
+from pybob.bob_tools import mkdir_p
 
-for im in glob('OIS*.tif'):
+mkdir_p('balanced')
+
+imlist = glob('OIS*.tif')
+imlist.sort()
+
+for im in imlist:
     print(im)
     img = imread(im)
     img_eq = (255 * equalize_adapthist(img)).astype(np.uint8)
     img_filt = median(img_eq, selem=disk(1))
-    imsave(im, img_filt)
+    imsave(os.path.join('balanced', im), img_filt)
