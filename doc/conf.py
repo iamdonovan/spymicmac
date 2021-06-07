58d064ff65c71a9802536a97f7b9c66ceba97041
Bob McNabb <robertmcnabb@gmail.com>
2021-06-07 13:47:03 +0100

    add rtree to conf.py mock imports

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,8 @@ autodoc_mock_imports = ['numpy', 'scipy', 'matplotlib', 'fiona', 'cv2', 'pyvips'
                         'shapely', 'opencv-python', 'pandas', 'geopandas', 'fiona.crs', 'scipy.interpolate',
                         'shapely.geometry.polygon', 'shapely.geometry', 'shapely.ops',  'shapely.strtree',
                         'skimage', 'gdal', 'h5py', 'pyproj', 'osgeo', 'llc', 'descartes', 'lxml.etree', 'ee',
-                        'pybob.landsat_tools', 'skimage.feature', 'skimage.morphology', 'lxml.builder']
+                        'pybob.landsat_tools', 'skimage.feature', 'skimage.morphology', 'skimage.filters',
+                        'lxml.builder', 'rtree']
 
 sys.modules.update((mod_name, Mock()) for mod_name in autodoc_mock_imports)
 sys.path.insert(0, os.path.abspath('..'))
