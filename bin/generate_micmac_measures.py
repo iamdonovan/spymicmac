efde22b69a9c3518be130d136e138b81d9d43034
Bob McNabb <robertmcnabb@gmail.com>
2021-06-18 15:16:37 +0100

    add description to argparser

--- bin/generate_micmac_measures.py
+++ bin/generate_micmac_measures.py
@@ -4,7 +4,8 @@ import sPyMicMac.micmac as mt
 
 
 def _argparser():
-    parser = argparse.ArgumentParser(description="", formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(description="Create id_fiducial.txt, MeasuresCamera.xml, and Tmp-SL-Glob.xml files for KH-9 Hexagon images.",
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-joined', action='store_true',
                         help='generate files for joined scene (220x460 mm) instead of half (220x230mm)')
     return parser
