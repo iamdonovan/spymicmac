ee7a850ec3a53537399011802d9ce2778a26e7e9
Bob McNabb <robertmcnabb@gmail.com>
2021-06-21 23:30:05 +0100

    migrate CLI scripts to entrypoints.

--- setup.py
+++ setup.py
@@ -12,13 +12,19 @@ setup(name='spymicmac',
                         'shapely', 'opencv-python', 'pandas', 'geopandas', 'pymmaster>0.1',
                         'scikit-image>=0.18', 'gdal', 'llc',
                         'sphinx-argparse', 'earthengine-api', 'pyasn1', 'usgs'],
-      scripts=['bin/balance_images.py', 'bin/combine_auto_measures.py',
-               'bin/generate_micmac_measures.py', 'bin/get_autogcp_locations.sh',
-               'bin/join_hexagon.py', 'bin/post_process_micmac.sh', 'bin/register_ortho.py',
-               'bin/remove_crosses.py', 'bin/resample_hexagon.sh'],
-      entry_points = {
+      scripts=['bin/get_autogcp_locations.sh',
+               'bin/post_process_micmac.sh',
+               'bin/resample_hexagon.sh'],
+      entry_points={
           'console_scripts': [
-                'find_reseau_grid = spymicmac.tools.find_reseau_grid:main'
+              'balance_images = spymicmac.tools.balance_images:main',
+              'combine_auto_measures = spymicmac.tools.combine_auto_measures:main',
+              'find_reseau_grid = spymicmac.tools.find_reseau_grid:main',
+              'generate_micmac_measures = spymicmac.tools.generate_micmac_measures:main',
+              'join_hexagon = spymicmac.tools.join_hexagon:main',
+              'move_bad_tapas = spymicmac.tools.move_bad_tapas:main',
+              'register_ortho = spymicmac.tools.register_ortho:main',
+              'remove_crosses = spymicmac.tools.remove_crosses:main'
           ],
       },
       zip_safe=False)
