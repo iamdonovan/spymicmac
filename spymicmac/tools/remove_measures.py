dde19c87a3471b512709a0939d4cefb1b0cc0bee
Bob McNabb <robertmcnabb@gmail.com>
2021-06-27 21:14:37 +0100

    remove extra 's' from arg name

--- spymicmac/tools/remove_measures.py
+++ spymicmac/tools/remove_measures.py
@@ -6,7 +6,7 @@ from spymicmac.micmac import remove_measure
 def _argparser():
     parser = argparse.ArgumentParser(description="Remove GCP(s) from a Measures xml file.",
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('fn_meas', action='store', type=str, nargs='+', help='xml file to remove GCP(s) from.')
+    parser.add_argument('fn_meas', action='store', type=str, help='xml file to remove GCP(s) from.')
     parser.add_argument('gcp', action='store', type=str, nargs='+', help='GCP name(s) to remove from <fn_meas>.')
     return parser
 
@@ -15,7 +15,8 @@ def main():
     parser = _argparser()
     args = parser.parse_args()
 
-    for gcp in args.gcps:
+    for gcp in args.gcp:
+
         remove_measure(args.fn_meas, gcp)
 
 
