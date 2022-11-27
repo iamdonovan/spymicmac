280233c84cee4cdfe5e95093f5d4c9690205d843
Bob McNabb <robertmcnabb@gmail.com>
2022-11-27 20:42:59 +0000

    add functions to generate additional xml files for kh-9 images

--- spymicmac/tools/generate_micmac_measures.py
+++ spymicmac/tools/generate_micmac_measures.py
@@ -4,7 +4,9 @@ import spymicmac.micmac as mt
 
 
 def _argparser():
-    parser = argparse.ArgumentParser(description="Create id_fiducial.txt, MeasuresCamera.xml, and Tmp-SL-Glob.xml files for KH-9 Hexagon images.",
+    parser = argparse.ArgumentParser(description="Create MicMac-LocalChantierDescripteur.xml, "
+                                                 "AutoCal_Foc-304800_KH9MC.xml, id_fiducial.txt, MeasuresCamera.xml, "
+                                                 "and Tmp-SL-Glob.xml files for KH-9 Hexagon Mapping Camera images.",
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-joined', action='store_true',
                         help='generate files for joined scene (220x460 mm) instead of half (220x230mm)')
@@ -14,6 +16,8 @@ def _argparser():
 def main():
     parser = _argparser()
     args = parser.parse_args()
+    mt.init_autocal()
+    mt.create_localchantier_xml()
     mt.generate_measures_files(joined=args.joined)
 
 
