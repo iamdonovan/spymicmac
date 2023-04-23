13c8bc50508663e43a84cf336e2052c23fc23de8
Bob McNabb <robertmcnabb@gmail.com>
2023-04-23 19:36:18 +0100

    add new modules to spymicmac.__init__

--- spymicmac/__init__.py
+++ spymicmac/__init__.py
@@ -1,8 +1,11 @@
 """
-sPyMicMac is a python package to help in the processing of KH-9 and historical aerial imagery using MicMac
+spymicmac is a python package to help in the processing of historical aerial imagery using MicMac
 """
+from . import data
 from . import image
 from . import micmac
+from . import matching
+from . import orientation
 from . import register
-from . import data
+from . import resample
 # from . import ee_tools
