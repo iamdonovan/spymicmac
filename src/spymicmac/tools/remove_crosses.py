501386c1054e94ec34689f7d12bf0a00b9f6353d
Bob McNabb <robertmcnabb@gmail.com>
2023-05-07 20:24:35 +0100

    add nproc argument

--- src/spymicmac/tools/remove_crosses.py
+++ src/spymicmac/tools/remove_crosses.py
@@ -7,6 +7,7 @@ def _argparser():
     parser = argparse.ArgumentParser(description="Remove Reseau marks from KH-9 image(s).",
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('img', action='store', type=str, nargs='+', help='Image(s) to remove crosses from.')
+    parser.add_argument('-n', '--nproc', type=int, default=1, help='number of sub-processes to use (default: 1).')
     return parser
 
 
@@ -16,7 +17,7 @@ def main():
 
     for im in args.img:
         print(im)
-        remove_crosses(im)
+        remove_crosses(im, nproc=args.nproc)
 
 
 if __name__ == "__main__":
